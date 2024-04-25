12769b7c304820648048b222e104b94bff70d791
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-04-25 23:54:57 +0530

    start.py

--- SlayerX/plugins/bot/start.py
+++ SlayerX/plugins/bot/start.py
@@ -42,11 +42,11 @@ async def start_pm(client, message: Message, _):
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    text=f"{message.from_user.mention} 𝐣𝐮𝐬𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐭𝐡𝐞 𝐘𝐚𝐬𝐡𝐢𝐤𝐚 𝐦𝐮𝐬𝐢𝐜 𝐛𝐨𝐭💗 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤 <b>𝐬𝐮𝐝𝐨𝐥𝐢𝐬𝐭</b>.\n\n<b>𝐮𝐬𝐞𝐫 𝐢𝐝 :</b> <code>{message.from_user.id}</code>\n<b>𝐮𝐬𝐞𝐫𝐧𝐚𝐦𝐞 :</b> @{message.from_user.username}",
                 )
             return
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
+            m = await message.reply_text("𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠💗")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
@@ -80,7 +80,7 @@ async def start_pm(client, message: Message, _):
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    text=f"{message.from_user.mention} 𝐉𝐮𝐬𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐓𝐡𝐞 𝐘𝐚𝐬𝐡𝐢𝐤𝐚 𝐌𝐮𝐬𝐢𝐜 𝐛𝐨𝐭💗 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤 <b>𝐭𝐫𝐚𝐜𝐤 𝐢𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧</b>.\n\n<b>𝐮𝐬𝐞𝐫 𝐢𝐝 :</b> <code>{message.from_user.id}</code>\n<b>𝐮𝐬𝐞𝐫𝐧𝐚𝐦𝐞 :</b> @{message.from_user.username}",
                 )
     else:
         out = private_panel(_)
@@ -92,7 +92,7 @@ async def start_pm(client, message: Message, _):
         if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                text=f"{message.from_user.mention} 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐓𝐡𝐞 𝐘𝐚𝐬𝐡𝐢𝐤𝐚 𝐌𝐮𝐬𝐢𝐜 bot💗.\n\n<b>𝐮𝐬𝐞𝐫 𝐢𝐝 :</b> <code>{message.from_user.id}</code>\n<b>𝐮𝐬𝐞𝐫𝐧𝐚𝐦𝐞 :</b> @{message.from_user.username}",
             )
 
 
